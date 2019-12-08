66f4a840e37d2dd7f1bea99b94cd6371a75ecef5
Marina Gjorgjievski <marina.gjorgjievski@outlook.com>
2019-12-08 21:59:24 +0700

    adding starting project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation.js';
+import Signin from './components/Signin/Signin.js';
+import Register from './components/Register/Register.js';
+import FoodRecognition from './components/FoodRecognition/FoodRecognition.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = new Clarifai.App({
+ apiKey: '70f5b5e032cb4b30a343beadd241fdcb'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
 }
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL: '',
+      ingredients: [],
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  calculateImageLocation = (data) => {
+
+    const clarifaiFood = data.outputs[0].data.concepts;
+    let ingredients = [];
+    clarifaiFood.forEach(function (item) {
+      ingredients.push(item.name);
+    })
+
+    return ingredients;
+  }
+  displayFoodBox = (ingredients) => {
+    this.setState({ingredients:ingredients});
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+  onButtonSubmit = () => {
+    this.setState({imageURL: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FOOD_MODEL, 
+        this.state.input
+      )
+    .then(response => this.displayFoodBox(this.calculateImageLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn:false})
+    } else if(route === 'home') {
+      this.setState({isSignedIn:true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const { isSignedIn, imageURL, route, ingredients } = this.state;
+    return (
+      <div className="App">
+        <Particles className = 'particles'
+          params={particlesOptions}
+        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home' 
+          ? <div>
+              <Logo/>
+              <Rank/>
+              <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit}/>
+              <FoodRecognition ingredients={ingredients} imageURL={imageURL}/>
+            </div>
+            : (
+                route === 'signin' 
+                ? <Signin onRouteChange={this.onRouteChange}/>
+                : <Register onRouteChange={this.onRouteChange}/>
+              )
+        }
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/components/FoodRecognition/FoodRecognition.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './FoodRecognition.css';
+
+const FoodRecognition = ({ingredients, imageURL}) => {
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img id='inputimage' alt='' src={imageURL} width='500px' heigh='auto'/>
+        <div className='concepts'>
+          <ul>
+            {ingredients.map((ingredient, index) =>
+              <li key={index}>
+                {ingredient}
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FoodRecognition;
