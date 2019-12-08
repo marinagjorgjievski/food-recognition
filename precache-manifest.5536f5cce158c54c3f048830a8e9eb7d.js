5fb14848c3fcf6e981651e30df1b506d7c21c920
Marina Gjorgjievski <marina.gjorgjievski@outlook.com>
2019-12-08 22:11:34 +0700

    Updates

---
+++ precache-manifest.5536f5cce158c54c3f048830a8e9eb7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b98eeb4c3611d72e5ecb498e492d82",
+    "url": "/food-recognition/index.html"
+  },
+  {
+    "revision": "1ee33ec27d5b0b43282b",
+    "url": "/food-recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "53845eba586418739b00",
+    "url": "/food-recognition/static/css/main.c2b172d9.chunk.css"
+  },
+  {
+    "revision": "1ee33ec27d5b0b43282b",
+    "url": "/food-recognition/static/js/2.9cbb10e5.chunk.js"
+  },
+  {
+    "revision": "53845eba586418739b00",
+    "url": "/food-recognition/static/js/main.5a901a74.chunk.js"
+  },
+  {
+    "revision": "68c20eeeb4e9e14579e3",
+    "url": "/food-recognition/static/js/runtime-main.b7ba0592.js"
+  }
+]);
